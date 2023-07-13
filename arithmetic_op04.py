404fe24ab9cd0d7e436f65f5f3869e7d3a516da7
FirdavsBoysunov <firdavsboysunov04@gmail.come>
2023-07-13 14:29:05 +0500

    turtinchi vazifa bajarildi

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,12 @@
 #Create a variable called 'number' and assign it the three-digit number.
-
+number=384
 #Find the 'number' first digit and assign to x1.
-
+x1=384//100
 #Find the 'number' second digit and assign to x2.
-
+x2=384%100//10
 #Find the 'number' third digit and assign to x3.
-
+x3=84%10
 #Create a variable called 'answer' and assign it the sum of the three digits x1, x2, x3.
-
+ansver=x1+x2+x3
 #Print the value of the 'answer.
+print(ansver)
